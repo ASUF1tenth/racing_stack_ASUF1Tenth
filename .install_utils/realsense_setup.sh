33ed9c3110b79bd2e49a6acf047b7e274337abc0
maubrunn <maubrunn@ethz.ch>
2026-07-15 15:34:35 +0200

    fix: add missing y

--- .install_utils/realsense_setup.sh
+++ .install_utils/realsense_setup.sh
@@ -8,7 +8,7 @@ gpg --dearmor | tee /etc/apt/keyrings/librealsenseai.gpg > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/librealsenseai.gpg] https://librealsense.realsenseai.com/Debian/apt-repo `lsb_release -cs` main" | \
 tee /etc/apt/sources.list.d/librealsense.list
 apt-get update
-apt-get install librealsense2-dkms
-apt-get install librealsense2-utils
-apt-get install librealsense2-dev
-apt-get install librealsense2-dbg
+apt-get -y install librealsense2-dkms
+apt-get -y install librealsense2-utils
+apt-get -y install librealsense2-dev
+apt-get -y install librealsense2-dbg
